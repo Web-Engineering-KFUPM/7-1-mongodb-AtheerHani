0a603e19f7416b164433aa06b4aeae86b37f882d
AtheerHani <atheer.almomtin@gmail.com>
2025-11-17 12:30:40 +0300

    lab commit

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -170,22 +170,51 @@
 import mongoose from "mongoose";
 
 // establish connection
+mongoose.connect("mongodb+srv://AtheerHani:Atheer9226@cluster0.cvyqumy.mongodb.net/")
+.then(() => console.log("Connected"))
+.catch(err => console.log(err));
 
 
 // define schema
 
 
-// create document
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  major: String
+});
 
+const Student = mongoose.model("Student", studentSchema);
+
+// create document
+async function createStudents() {
+      await Student.insertMany([
+         { name: "Ali", age: 21, major: "CS" },
+         { name: "Sara", age: 23, major: "SE" }
+      ]);
+      console.log("✅ Inserted");
+      }
+createStudents();
 
 // read document
-
+async function readStudents() {
+         const all = await Student.find();
+         console.log(all);
+      }
+readStudents();
 
 // update document
-
+async function updateStudent() {
+         await Student.updateOne({ name: "Ali" }, { age: 22 });
+         console.log("✅ Updated Ali");
+      }
 
 // delete document
+async function deleteStudent() {
+         await Student.deleteOne({ name: "Sara" });
+      console.log("✅ Deleted Sara");
+      }
 
-
-
-
+      
+updateStudent();
+deleteStudent(); 
